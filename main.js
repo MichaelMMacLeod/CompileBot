c23f7eb060889ec43638c68a891f48e42aef6023
Michael MacLeod <michaelmmacleod@gmail.com>
2017-09-13 14:47:39 -0700

    Refactor code

--- main.js
+++ main.js
@@ -8,126 +8,115 @@ var bot = new Discord.Client({
     autorun: true
 });
 
-var MESSAGE_TYPES = {
-    QUERY: 'query',
-    CODE: 'compilation'
+/* Checks to see if a String starts with @CompileBot
+ *
+ * String s       | A String.
+ * return Boolean | true; The message starts with @CompileBot
+ *                  false; The message does not start with @CompileBot
+ */
+function containsCall(s) {
+    return s.startsWith('@<' + bot.id + '>');
 }
 
-var parseUserMessage = function (message) {
-    if (message.substring(0, ('<@' + bot.id + '>').length) != '<@' + bot.id + '>')
-        return {valid:false};
+/* Converts the name of a language to it's rextester.com id.
+ *
+ * String l      | The name of the language.
+ * return Number | The rextester.com language id.
+ */
+function parseLanguage(l) {
+    var langs = {
+        'C#': 1,
+        'VB.NET': 2,
+        'F#': 3,
+        'Java': 4,
+        'Python': 5,
+        'C (gcc)': 6,
+        'C++ (gcc)': 7,
+        'Php': 8,
+        'Pascal': 9,
+        'Objective-C': 10,
+        'Haskell': 11,
+        'Ruby': 12,
+        'Perl': 13,
+        'Lua': 14,
+        'Nasm': 15,
+        'Sql Server': 16,
+        'Javascript': 17,
+        'Lisp': 18,
+        'Prolog': 19,
+        'Go': 20,
+        'Scala': 21,
+        'Scheme': 22,
+        'Node.js': 23,
+        'Python 3': 24,
+        'Octave': 25,
+        'C (clang)': 26,
+        'C++ (clang)': 27,
+        'C++ (vc++)': 28,
+        'C (vc)': 29,
+        'D': 30,
+        'R': 31,
+        'Tcl': 32,
+        'MySQL': 33,
+        'PostgreSQL': 34,
+        'Oracle': 35,
+        'Swift': 37,
+        'Bash': 38,
+        'Ada': 39,
+        'Erlang': 40,
+        'Elixer': 41,
+        'Ocaml': 42,
+        'Kotlin': 43,
+        'Brainfuck': 44,
+        'Fortran': 45
+    };
 
-    var text = message;
-    var lines = (function () {
-        return text.split('\n');
-    })();
-    var type = (function () {
-        if (lines.length === 1)
-            return MESSAGE_TYPES.QUERY;
-        else
-            return MESSAGE_TYPES.CODE;
-    })();
-    var valid = (function () {
-        if (lines.length === 0)
-            return false;
+    return langs[l];
+}
 
-        var botName   = '<@' + bot.id + '>'
-          , givenName = lines[0].substring(0, botName.length);
+/* Parses a valid compilation query. The query must use proper syntax.
+ *
+ * String m  | The entire user message.
+ * return {} | An object specifying the language, program, input, and compiler
+ *             args of the user's message all stored as Strings.
+ */
+function parseMessage(m) {
+    var lines   = m.split('\n')
+      , args    = lines[0].match(/-\w+/g)
+      , lang    = lines[0].match(/\w+\n/g)
+      , content = m.match(/\n[^```]*/g)
+      , program = content[0]
+      , input   = content[1];
 
-        if (botName != givenName)
-            return false;
+    return {
+        'LanguageChoice': parseLanguage(lang),
+        'Program': program,
+        'Input': input,
+        'CompilerArgs': args
+    };
+}
 
-        if (type == MESSAGE_TYPES.QUERY && lines.length != 1)
-            return false;
+/* Sends an api POST request to http://rextester.com/
+ * 
+ * String m      | A valid message.
+ * return String | Rextester's response.
+ */
+function rextest(m) {
+    request.post(
+        'http://rextester.com/rundotnet/api',
+        { json: parseMessage(m) },
+        function (e, response, body) {
+            return body['Result'];
+        });
+}
 
-        if (type == MESSAGE_TYPES.CODE && lines.length < 2)
-            return false;
-
-        // TODO: more checks needed!
-
-        return true;
-    })();
-    var content = (function () {
-        if (type === MESSAGE_TYPES.CODE) {
-            return text.match(/```[\S\s]*```/g)[0];
-        } else {
-            return "Hi, I'm CompileBot. Call me *exactly* like this:\n\n    @CompileBot **Language**\\`\\`\\`\n    **Code**\n    \\`\\`\\`\nFor example,\n\n    @CompileBot **Java**\\`\\`\\`\n**    class Rextester\n    {\n        public static void main(String[] args)\n        {\n            System.out.println(\"Hello, World!\");\n        }\n    }**\n    \\`\\`\\`";
-        }
-    })();
-    var codeLines = (function () {
-        if (type === MESSAGE_TYPES.CODE)
-            return content.split('\n');
-        else
-            return '';
-    })();
-    var program = (function () {
-        if (type === MESSAGE_TYPES.QUERY)
-            return '';
-        
-        var code = '';
-
-        for (let i = 1; i < codeLines.length - 1; i++) {
-            code += codeLines[i];
-        }
-
-        return code;
-    })();
-    var input = ''; // TODO
-    var language = (function () {
-        if (type === MESSAGE_TYPES.CODE) {
-            var langs = {
-                'C#': 1,
-                'VB.NET': 2,
-                'F#': 3,
-                'Java': 4,
-                'Python': 5,
-                'C (gcc)': 6,
-                'C++ (gcc)': 7,
-                'Php': 8,
-                'Pascal': 9,
-                'Objective-C': 10,
-                'Haskell': 11,
-                'Ruby': 12,
-                'Perl': 13,
-                'Lua': 14,
-                'Nasm': 15,
-                'Sql Server': 16,
-                'Javascript': 17,
-                'Lisp': 18,
-                'Prolog': 19,
-                'Go': 20,
-                'Scala': 21,
-                'Scheme': 22,
-                'Node.js': 23,
-                'Python 3': 24,
-                'Octave': 25,
-                'C (clang)': 26,
-                'C++ (clang)': 27,
-                'C++ (vc++)': 28,
-                'C (vc)': 29,
-                'D': 30,
-                'R': 31,
-                'Tcl': 32,
-                'MySQL': 33,
-                'PostgreSQL': 34,
-                'Oracle': 35,
-                'Swift': 37,
-                'Bash': 38,
-                'Ada': 39,
-                'Erlang': 40,
-                'Elixer': 41,
-                'Ocaml': 42,
-                'Kotlin': 43,
-                'Brainfuck': 44,
-                'Fortran': 45
-            };
-            return +langs[codeLines[0].substring(3, codeLines[0].length)];
-        } else {
-            return '';
-        }
-    })();
-
-    return {text, lines, type, valid, content, program, input, language};
+/* Sends a channel message.
+ * 
+ * String id | id of the channel.
+ * String m  | Message to send.
+ */
+function sendMessage(id, m) {
+    bot.sendMessage({ to: id, message: m });
 }
 
 bot.on('ready', function (e) {
@@ -135,40 +124,13 @@ bot.on('ready', function (e) {
     logger.info("Logged in as: ");
     logger.info(bot.username + " - (" + bot.id + ")");
 });
+
 bot.on('message', function (user, userID, channelID, message, e) {
-    var parsed = parseUserMessage(message);
-    if (parsed.valid) {
-        switch (parsed.type) {
-            case MESSAGE_TYPES.QUERY:
-                bot.sendMessage({
-                    to: channelID,
-                    message: parsed.content
-                });
-                break;
-            case MESSAGE_TYPES.CODE:
-                console.log(parsed.language);
-                request.post(
-                    'http://rextester.com/rundotnet/api',
-                    {
-                        json: {
-                            'LanguageChoice': parsed.language,
-                            'Program': parsed.program,
-                            'Input': parsed.input,
-                            'CompilerArgs': parsed.args
-                        }
-                    },
-                    function (e, response, body) {
-                        var r = '`Output`\n' + body['Result'] + '\n`Errors`\n' + body['Errors'];
-                        console.log(r);
-                        bot.sendMessage({
-                            to: channelID,
-                            message: r
-                        })
-                    });
-                break;
-            default:
-                console.log("Unknown message type: " + parsed.type);
-                break;
+    if (containsCall(message)) {
+        try {
+            sendMessage(channelID, rextest(message));
+        } catch (e) {
+            sendMessage(channelID, e.message);
         }
     }
 });
