7003d95fc5cf8e9df20f7c6d351f49532301cb3b
Michael MacLeod <michaelmmacleod@gmail.com>
2017-09-13 23:16:10 -0700

    Fix bot not correctly sending language in POST request

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var bot = new Discord.Client({
  *                  false; The message does not start with @CompileBot
  */
 function containsCall(s) {
-    return s.startsWith('@<' + bot.id + '>');
+    return s.startsWith('<@' + bot.id + '>');
 }
 
 /* Converts the name of a language to it's rextester.com id.
@@ -25,50 +25,50 @@ function containsCall(s) {
  */
 function parseLanguage(l) {
     var langs = {
-        'C#': 1,
-        'VB.NET': 2,
-        'F#': 3,
-        'Java': 4,
-        'Python': 5,
-        'C (gcc)': 6,
-        'C++ (gcc)': 7,
-        'Php': 8,
-        'Pascal': 9,
-        'Objective-C': 10,
-        'Haskell': 11,
-        'Ruby': 12,
-        'Perl': 13,
-        'Lua': 14,
-        'Nasm': 15,
-        'Sql Server': 16,
-        'Javascript': 17,
-        'Lisp': 18,
-        'Prolog': 19,
-        'Go': 20,
-        'Scala': 21,
-        'Scheme': 22,
-        'Node.js': 23,
-        'Python 3': 24,
-        'Octave': 25,
-        'C (clang)': 26,
-        'C++ (clang)': 27,
-        'C++ (vc++)': 28,
-        'C (vc)': 29,
-        'D': 30,
-        'R': 31,
-        'Tcl': 32,
-        'MySQL': 33,
-        'PostgreSQL': 34,
-        'Oracle': 35,
-        'Swift': 37,
-        'Bash': 38,
-        'Ada': 39,
-        'Erlang': 40,
-        'Elixer': 41,
-        'Ocaml': 42,
-        'Kotlin': 43,
-        'Brainfuck': 44,
-        'Fortran': 45
+        'C#': '1',
+        'VB.NET': '2',
+        'F#': '3',
+        'Java': '4',
+        'Python': '5',
+        'C(gcc)': '6',
+        'C++(gcc)': '7',
+        'Php': '8',
+        'Pascal': '9',
+        'Objective-C': '10',
+        'Haskell': '11',
+        'Ruby': '12',
+        'Perl': '13',
+        'Lua': '14',
+        'Nasm': '15',
+        'SqlServer': '16',
+        'Javascript': '17',
+        'Lisp': '18',
+        'Prolog': '19',
+        'Go': '20',
+        'Scala': '21',
+        'Scheme': '22',
+        'Node.js': '23',
+        'Python 3': '24',
+        'Octave': '25',
+        'C(clang)': '26',
+        'C++(clang)': '27',
+        'C++(vc++)': '28',
+        'C(vc)': '29',
+        'D': '30',
+        'R': '31',
+        'Tcl': '32',
+        'MySQL': '33',
+        'PostgreSQL': '34',
+        'Oracle': '35',
+        'Swift': '37',
+        'Bash': '38',
+        'Ada': '39',
+        'Erlang': '40',
+        'Elixer': '41',
+        'Ocaml': '42',
+        'Kotlin': '43',
+        'Brainfuck': '44',
+        'Fortran': '45'
     };
 
     return langs[l];
@@ -81,33 +81,32 @@ function parseLanguage(l) {
  *             args of the user's message all stored as Strings.
  */
 function parseMessage(m) {
-    var lines   = m.split('\n')
-      , args    = lines[0].match(/-\w+/g)
-      , lang    = lines[0].match(/\w+\n/g)
-      , content = m.match(/\n[^```]*/g)
-      , program = content[0]
-      , input   = content[1];
+    var lines   = m.split('\n') || ''
+      , args    = lines[0].match(/-.[^-^`]+/g) || []
+      , lang    = lines[0].match(/\w+$/g) || ''
+      , content = m.match(/\n[^```]*/g) || ''
+      , program = content[0] || ''
+      , input   = content[1] || '';
 
     return {
         'LanguageChoice': parseLanguage(lang),
         'Program': program,
         'Input': input,
-        'CompilerArgs': args
+        'CompilerArgs': args.join(' ')
     };
 }
 
 /* Sends an api POST request to http://rextester.com/
  * 
  * String m      | A valid message.
+ * Function f    | This function is called with the response to the request.
  * return String | Rextester's response.
  */
-function rextest(m) {
+function rextest(m, f) {
     request.post(
         'http://rextester.com/rundotnet/api',
         { json: parseMessage(m) },
-        function (e, response, body) {
-            return body['Result'];
-        });
+        f);
 }
 
 /* Sends a channel message.
@@ -128,9 +127,16 @@ bot.on('ready', function (e) {
 bot.on('message', function (user, userID, channelID, message, e) {
     if (containsCall(message)) {
         try {
-            sendMessage(channelID, rextest(message));
+            rextest(
+                message, 
+                function (e, response, body) {
+                    if (body['Errors'])
+                        sendMessage(channelID, 'rextester error(s):```\n' + body['Errors'] + '```');
+                    else
+                        sendMessage(channelID, 'Output:```\n' + body['Result'] + '```');
+                });
         } catch (e) {
-            sendMessage(channelID, e.message);
+            sendMessage(channelID, 'Error in sending/parsing request:```\n' + e.message + '```');
         }
     }
 });
